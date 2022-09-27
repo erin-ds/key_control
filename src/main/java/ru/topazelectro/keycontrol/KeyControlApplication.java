32a962390aa63bcb409cb8674fff0e0ea2523a05
erin_ds <erin_ds@topazelectro.ru>
2022-09-27 10:21:23 +0300

    init

---
+++ src/main/java/ru/topazelectro/keycontrol/KeyControlApplication.java
@@ -0,0 +1,13 @@
+package ru.topazelectro.keycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyControlApplication.class, args);
+	}
+
+}
