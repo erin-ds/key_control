280fad638f25f167d4e6f2f40a8edffd25d16da9
GHOST <raumzzz@yandex.ru>
2022-10-19 16:00:19 +0300

    fix update method

--- src/main/java/ru/topazelectro/keycontrol/dto/CommonDto.java
+++ src/main/java/ru/topazelectro/keycontrol/dto/CommonDto.java
@@ -4,9 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class CommonDto implements Serializable {
 
     @Schema(description = "Идентификатор записи", accessMode = Schema.AccessMode.READ_ONLY)
@@ -14,4 +18,16 @@ public abstract class CommonDto implements Serializable {
     @Schema(description = "Комментарий к записи")
     String comment;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommonDto commonDto = (CommonDto) o;
+        return id.equals(commonDto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
