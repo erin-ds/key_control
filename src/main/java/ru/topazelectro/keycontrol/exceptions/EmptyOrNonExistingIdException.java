179872ea91274509d81d87508ab4af9ac60fb682
GHOST <raumzzz@yandex.ru>
2022-11-14 10:38:06 +0300

    fix orthographic error

--- src/main/java/ru/topazelectro/keycontrol/exceptions/EmptyOrNonExistingIdException.java
+++ src/main/java/ru/topazelectro/keycontrol/exceptions/EmptyOrNonExistingIdException.java
@@ -2,7 +2,7 @@ package ru.topazelectro.keycontrol.exceptions;
 
 public class EmptyOrNonExistingIdException extends RuntimeException {
 
-    private static final String MESSAGE = "Передан несуществующий в ID или переданный id = null";
+    private static final String MESSAGE = "Передан несуществующий ID или переданный id = null";
 
     public EmptyOrNonExistingIdException() {
         super(MESSAGE);
